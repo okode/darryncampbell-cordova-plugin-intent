65b69ded5cbff3a17f60a9db64f9c6f3aff3ef62
psabater <psabater@okode.com>
2017-07-19 08:40:13 +0200

    Error with duplicated FileProvider in manifest.xml with Cordova

---
+++ src/android/FileProvider.java
@@ -0,0 +1,8 @@
+package com.darryncampbell.cordova.plugin.intent;
+
+/*
+ * http://stackoverflow.com/questions/40746144/error-with-duplicated-fileprovider-in-manifest-xml-with-cordova/41550634#41550634
+ */
+public class FileProvider extends android.support.v4.content.FileProvider {
+}
+
